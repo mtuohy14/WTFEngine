c3131795374b0c4372be0b031fb2fa400cef3aae
Jake Bellacera <jakeb@eleveninc.com>
2014-02-28 18:29:37 -0800

    Fixing google docs key regex

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@
 $(function() {
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AoCe6lq_0U-_dEhQTHlRT0VDTGtVZWhIa21iQUdDVnc' );
+    WTF.init('0AoCe6lq_0U-_dEhQTHlRT0VDTGtVZWhIa21iQUdDVnc');
 
 });
--- scripts/wtf.js
+++ scripts/wtf.js
@@ -13,7 +13,7 @@ var WTF = (function() {
 
     var RE_QUOTE = /\"([^\"]+)\"/gi;
     var RE_JSON = /\.json$/i;
-    var RE_KEY = /[a-z0-9]{32,}/i;
+    var RE_KEY = /[a-z0-9_-]{32,}/i;
     var DOCS_PATH = 'https://docs.google.com/spreadsheet/pub?key={key}&output=csv';
 
     var templates;
@@ -238,6 +238,7 @@ var WTF = (function() {
         */
 
         init: function( data ) {
+            console.log(data);
 
             if ( !data ) throw data + ' is not a valid corpus';
 
